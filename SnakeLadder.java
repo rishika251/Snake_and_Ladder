e2b4b6bad6b434471542fa8f37a3c9b43a7bd53e
rishika251 <rishikasadineni@gmail.com>
2022-11-09 13:57:52 +0530

    Added:Player then checks for a option

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,24 +1,73 @@
 package com.bridgelabz.snakeladder;
 
 public class SnakeLadder {
-	public static void main(String args[]) {
-	//UC1= Snake and Ladder game played with single player at start position 0
-    byte playerPosition = 0;
-    byte diceRoll;
-
+	 //UC1= Snake and Ladder game played with single player at start position 0
+   static  byte playerPosition = 0;
+    static byte diceRoll;
+    static byte SnakeArray[] = {54, 90, 99};
+    static byte LadderArray[] = {9, 40, 67};
+public static void main(String args[]) {
     System.out.println("Starting Position of the Player = " + playerPosition);
-    /*
+
     //UC2=The Player rolls the die to get a number between 1 to 6.
     diceRoll = (byte) ((byte) (Math.random() * 6) + 1);
-    System.out.println("Dice Number =" + " " + diceRoll);
-     */
+    System.out.println("Starting Position of the Player = 0");
 
-  //UC2=The Player rolls the die to get a number between 1 to 6.
-    diceRoll = (byte) ((byte) (Math.random() * 6) + 1);
-
+    for (int playerPosition = 1; playerPosition <= 100; playerPosition++) {
+        diceRoll = (byte) ((byte) (Math.random() * 6));
 
     System.out.println("Dice Number =" + " " + diceRoll);
-
+        System.out.println("Dice Number =" + " " + diceRoll);
 
 }
+        playerPosition = (byte) (playerPosition + diceRoll);
+        playerPosition = (byte) (playerPosition - 1);
+
+        System.out.println("Current Position =" + " " + playerPosition);
+
+        if (diceRoll == 0) {
+
+            playerPosition = (byte) (playerPosition - diceRoll);
+
+            System.out.println("You have to stay on the same position!!");
+        }
+
+        if (playerPosition == SnakeArray[0]) {
+            playerPosition = 19;
+
+            System.out.println("IT'S A SNAKE.");
+            System.out.println("You are at " + playerPosition + " position.");
+
+        } else if (playerPosition == SnakeArray[1]) {
+            playerPosition = 48;
+
+            System.out.println("IT'S A SNAKE.");
+            System.out.println("You are at " + playerPosition + " position.");
+
+        } else if (playerPosition == SnakeArray[2]) {
+            playerPosition = 77;
+
+            System.out.println("IT'S A SNAKE.");
+            System.out.println("Now, You are at " + playerPosition + " position.");
+
+        } else if (playerPosition == LadderArray[0]) {
+            playerPosition = 34;
+
+            System.out.println("YOU GOT A LADDER.");
+            System.out.println("You are at " + playerPosition + " position.");
+
+        } else if (playerPosition == LadderArray[1]) {
+            playerPosition = 64;
+
+            System.out.println("YOU GOT A LADDER.");
+            System.out.println("You are at " + playerPosition + " position.");
+
+        } else if (playerPosition == LadderArray[2]) {
+            playerPosition = 86;
+
+            System.out.println("YOU GOT A LADDER.");
+            System.out.println("You are at " + playerPosition + " position.");
+        }
+    }
+
 }
