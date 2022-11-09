159323975e190ee86529c906852d34eafb993199
rishika251 <rishikasadineni@gmail.com>
2022-11-09 11:33:17 +0530

    Added:ability to find player position starts with 0

---
+++ SnakeLadder.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.snakeladder;
+
+public class SnakeLadder {
+	public static void main(String args[]) {
+	//UC1= Snake and Ladder game played with single player at start position 0
+    byte playerPosition = 0;
+    byte diceRoll;
+
+    System.out.println("Starting Position of the Player = " + playerPosition);
+    /*
+    //UC2=The Player rolls the die to get a number between 1 to 6.
+    diceRoll = (byte) ((byte) (Math.random() * 6) + 1);
+    System.out.println("Dice Number =" + " " + diceRoll);
+     */
+
+
+
+}
+}
